4f065a02a8235c6744768328af5c1e103321ed44
Pagante <55301195+Pagante@users.noreply.github.com>
2021-06-27 22:47:28 +0100

    first commit

---
+++ accounts/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Account, UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
+
+
+# Register your models here.
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'last_login', 'is_active', 'date_joined')
+    list_display_links = ('email', 'last_name', 'first_name')
+    readonly_fields = ('last_login', 'date_joined')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    def Thumbnail(self, object):
+        return format_html("<img src='{}' width='30' style='border-radius: 50%;' />".format(object.profile_picture.url))
+
+    Thumbnail.short_description = 'Profile Picture'
+    list_display = ('Thumbnail', 'user', 'city', 'state', 'country')
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
